104fc20dc029ffee1e96a3ac968df777950edc55
Fausto Gregorio <fausto.g03@gmail.com>
2021-01-25 21:43:45 -0600

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  facebookAppId: '492470980769372',
-  facebookPostsToken: 'EAA5Rq3xHZCMABAE0mI8xIiMs5vwZAATT3o4b6cjE8P9ZA7rChdH49tSSFGmZBSyve7JjehRdSHogBULHVZAM5XYQDw0hhoDkTQD0FbbrAZB5jHWj3sPKPIUlVkqmkvdF6SlnAQWlH2z0gzMdKUYbvCA6u4ZBTX3NBVGSLNjBXLFhatXe6B7YQ9B'
+  facebookAppId: '492470980769372'
 };
